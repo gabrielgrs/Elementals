fd8e5f17147304546130c74baa662c36374c48e8
Gabriel Goncalves Ribeiro <ggribeiro@stefanini.com>
2017-11-22 20:10:29 -0200

    add create character scene

---
+++ src/Assets/Scripts/UI/CreatePlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatePlayer : MonoBehaviour {
+
+    public string Name;
+    public string Element;
+
+    public InputField nameField;
+    public Dropdown elementDropdown;
+
+
+	void Start () 
+    {
+	}
+
+    public void SavePlayer()
+    {
+        if (nameField.text != "")
+        {
+            Name = nameField.text;
+            PlayerPrefs.SetString("Name", Name);
+        }
+        else
+        {
+            print("Nome inválido!");
+        }
+        
+
+        if (elementDropdown.value.ToString() != "")
+        {
+            Element = elementDropdown.value.ToString();
+            PlayerPrefs.SetString("Element", Element);
+        }
+        else
+        {
+            print("Elemento inválido!");
+        }
+
+        if (Name != "" && Element != "")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Cidade");
+        }
+    }
+
+    public void CancelCreation()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MenuInicial");
+    }
+}
--- src/Assets/Scripts/UI/MenuInicial.cs
+++ src/Assets/Scripts/UI/MenuInicial.cs
@@ -6,8 +6,11 @@ public class MenuInicial : MonoBehaviour {
 
 	public GUISkin PerSkin;
 
+    public GameStorage gameStorage;
+
 	void Start () {
-		
+        gameStorage = gameObject.AddComponent<GameStorage>();
+        gameStorage.loadGame();
 	}
 	
 	void Update () {
@@ -19,7 +22,15 @@ public class MenuInicial : MonoBehaviour {
 		GUI.skin = PerSkin;
 
 		if (GUI.Button (new Rect ((float)Screen.width / 2.1f, Screen.height / 2 + 90, 90, 60), "Começar")) {
-			UnityEngine.SceneManagement.SceneManager.LoadScene ("Cidade");
+            if (PlayerPrefs.GetString("Name") == "")
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("CreatePlayer");
+            }
+            else
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Cidade");
+            }
+            
 		}
 
 		if (GUI.Button (new Rect ((float)Screen.width / 2.1f, Screen.height / 2 + 150, 90, 60), "Opções")) {
