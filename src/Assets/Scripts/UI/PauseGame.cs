1b1595fd39767b87d54ae66bc462caf3713d8db0
patym <patriciamachado1993@gmail.com>
2017-11-25 03:48:40 -0200

    pause menu

---
+++ src/Assets/Scripts/UI/PauseGame.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public GameObject PausePanel;
+
+	private bool paused = false;
+
+	void Start() {		
+
+		PausePanel.SetActive (false);
+	}
+
+	void Update() {
+
+		if (Input.GetButtonDown ("Pause")) {
+			paused = !paused;
+		}
+
+		if (paused) {
+			PausePanel.SetActive (true);
+			Time.timeScale = 0;
+			AudioListener.volume = 0;
+		}
+
+		if (!paused) {
+			PausePanel.SetActive (false);
+			Time.timeScale = 1;
+			AudioListener.volume = 1;
+		}
+	}
+
+	public void Resume() {		
+
+		paused = false;
+	}
+
+	public void Restart() {
+
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void MainMenu() {
+
+		Application.LoadLevel (0);
+	}
+
+	public void Quit() {
+
+		Application.Quit();
+	}
+}
+
